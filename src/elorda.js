1a35bf9a6f9ef112fbfd219f2a6ec090e0c7a77f
maxim <maximum454@gmail.com>
2019-07-22 12:19:29 +0600

    fix

--- src/elorda.js
+++ src/elorda.js
@@ -12,9 +12,21 @@ import './img/partners-img.png'
 import './img/box-img-1.jpg'
 import './img/box-img-2.jpg'
 import './img/partners-logo.png'
+import './img/map2.jpg'
+import './img/map-slider.png'
 
 
 $('.js-partners').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true
+});
+
+$('.js-map-slider').slick({
     dots: false,
     arrows: true,
     infinite: true,
